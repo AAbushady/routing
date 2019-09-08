18daecb5ae29255669520865aad01e26ab646d26
Alexander Abushady <aabushady214@gmail.com>
2019-09-08 16:50:01 -0400

    Update routing.js
    Added problem statement.

---
+++ routing.js
@@ -0,0 +1,38 @@
+/*
+St. Petersburg College allows its employee to set up automatic deposit from their
+job. As such, the employee needs to specify the routing number for their bank. In
+order to make sure the money gets routed correctly, SPC wants to validate that
+the employee enters a valid routing number. The list of valid routing numbers is
+below.
+
+Region                  Routing/Transit Number
+Arizona                 122100024
+California              322271627
+Colorado                102001017
+Connecticut             021100361
+Florida                 267084131
+Georgia                 061092387
+Idaho                   123271978
+Illinois                071000013
+Indiana                 074000010
+Kentucky                083000237
+Louisiana               065400137
+Michigan                072000326
+Nevada                  322271627
+New Jersey              021202337
+New York - Downstate    021000021
+New York - Upstate      022300173
+Ohio                    044000037
+Oklahoma                103000648
+Oregon                  325070760
+Texas                   111000614
+Utah                    124001545
+Washington              325070760
+West Virginia           051900366
+Wisconsin               075000019
+
+These numbers should be stored in an array. Iterate over the array and
+determine if the entered routing number is in the array. If the number is in the
+array, display "valid". If the routing number is not an array, indicate that it is
+invalid.
+*/
